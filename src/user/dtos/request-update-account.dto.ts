256776eb92f3c1f27a202271243e405d26a6f6fa
Sebastián <seba.chavez21@gmail.com>
2023-08-09 01:00:43 -0400

    HU ok

--- src/user/dtos/request-update-account.dto.ts
+++ src/user/dtos/request-update-account.dto.ts
@@ -5,14 +5,14 @@ export class RequestUpdateAccountDto {
 
     @IsNotEmpty({message: Message.IS_NOT_EMPTY_DTO.replace('{{value}}', 'password')})
     @IsNumber({}, {message: Message.IS_NUMBER_DTO.replace('{{value}}','account_id')})
-    account_id: number;
+    readonly account_id: number;
 
     @IsNotEmpty({message: Message.IS_NOT_EMPTY_DTO.replace('{{value}}', 'password')})
     @IsString({message: Message.IS_STRING_DTO.replace('{{value}}', 'password')})
     @MinLength(8, {message: Message.MIN_LENGTH_DTO.replace('{{value}}', 'password').replace('{{length}}', '8')})
-    password: string;
+    readonly password: string;
     
     @IsNotEmpty({message: Message.IS_NOT_EMPTY_DTO.replace('{{value}}', 'genre')})
     @IsString({message: Message.IS_STRING_DTO.replace('{{value}}', 'genre')})
-    genre: string;
+    readonly genre: string;
 }
